b3ac4a23f97ffa16d25da2808d801f40625685bb
Alex Helo <thealexxd25@hotmail.com>
2020-03-09 21:07:06 -0600

    added better controls and started working on grounding method

--- Figthing Platformer/Assets/Scripts/PlayerController.cs
+++ Figthing Platformer/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1.0f;
+
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
 
-    public float jumpVelocity = 5f;
-    public float fallMultiplier = 2.5f;
-    public float lowJumpMultiplier = 2f;
+
+    [Space]
+    [Header("Stats")]
+    public float moveSpeed = 5.0f;
+    public float jumpVelocity = 10f;
+    public float fallMultiplier = 4f;
+    public float lowJumpMultiplier = 3f;
+    public int numJumps = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -22,22 +27,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            spriteRenderer.flipX = false;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
-            spriteRenderer.flipX = true;
-        }
+        //Movement
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+        Walk(dir);
 
-        if (Input.GetKeyDown(KeyCode.W))
+        //Better Jumping
+        if (Input.GetButtonDown("Jump") && numJumps > 0)
         {
-            rb.velocity = Vector2.up * jumpVelocity;
-        }
 
+            Jump(Vector2.up);
+            numJumps--;
+        }
         if (rb.velocity.y < 0)
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
@@ -46,6 +48,21 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
+
+
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity = new Vector2(dir.x * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump(Vector2 dir)
+    {
+
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpVelocity;
+
 
     }
 }
