6b380bd3e7af7f2788b66666f0fdca3fdf57d816
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2020-03-10 18:35:54 -0600

    Add platform
    Try out, pushing in unity, added a platform and a main player

--- Figthing Platformer/Assets/Scripts/PlayerController.cs
+++ Figthing Platformer/Assets/Scripts/PlayerController.cs
@@ -65,4 +65,11 @@ public class PlayerController : MonoBehaviour
 
 
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            numJumps = 2; 
+        }
+    }
 }
