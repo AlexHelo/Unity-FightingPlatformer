e160cef99588cd522cfb8f9ed7c789300902c9c3
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2020-03-29 20:20:41 -0600

    Reorganize player controller and player attack scripts

--- Figthing Platformer/Assets/Scripts/PlayerAttack/PlayerAttack.cs
+++ Figthing Platformer/Assets/Scripts/PlayerAttack/PlayerAttack.cs
@@ -1,39 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * GLOSARY:
+ *		1) VALUES
+ *		2) START METHOD
+ *		3) UPDATE METHOD
+ *		4) FIXED UPDATE
+ *		5) ATTACK METHOD
+ *		6) IENUMERATOR
+ *		7) AUXILIARY METHODS
+ *		8) GRIZMOS
+ * 
+ */ 
 public class PlayerAttack : MonoBehaviour
 {
+	public Transform attackPos;
+	public LayerMask whatIsEnemies;
 
-    private float timeBtWAttack,timeResetAttack;
-    public float startTimeBtwAttack,startTimeResetAttack;
-    private bool Pressed = false;
+	private Rigidbody2D rigidbody;
 
-    public Transform attackPos;
-    public float attackRange,attackRange2,attackRange3;
-    public LayerMask whatIsEnemies;
+	private float timeBtWAttack,timeResetAttack;
+	private float force;
 
-    private Animator an;
-    float totalDuration;
-    public float damage;
-    private Rigidbody2D rigidbody;
-    private int attackNum;
-    private bool startTimer=false;
-    private PlayerController pC;
-    private float force;
-    void Start()
+	public float startTimeBtwAttack,startTimeResetAttack;
+	public float attackRange, attackRange2, attackRange3;
+	public float damage;
+
+	float totalDuration;
+
+	private bool Pressed = false;
+	private bool startTimer = false;
+
+	private int attackNum;
+    
+	private Animator an;
+	private PlayerController pC;
+	
+	void Start()
     {
         an = GetComponent<Animator>();
-        //ParticleSystem parts = particles.GetComponent<ParticleSystem>();
-        //totalDuration = parts.duration + parts.startLifetime;
         rigidbody = GetComponent<Rigidbody2D>();
         pC = GetComponent<PlayerController>();
     }
-
-
+	/*
+	 *	UPDATE METHOD
+	 *		1) TIME BETWEEN ATTACKS
+	 */ 
+	
     void Update()
     {
-
+		// Check to substract time between attacks
         if (timeBtWAttack >= 0)
         {
             timeBtWAttack -= Time.deltaTime;
@@ -43,156 +60,144 @@ public class PlayerAttack : MonoBehaviour
             timeResetAttack -= Time.deltaTime;
         }
        
-
     }
+	/*
+	 * FIXED UPDATE METHDOD
+	 *		1) RESET COMBO LOSS
+	 *		2) DETERMINE ATTACK POSITION
+	 */ 
     private void FixedUpdate()
     {
+		//check to reset cooldown attack if combo is loss 
         if (timeResetAttack <= 0)
         {
             ResetCooldown();
         }
+		//Establish the attacking point
         attackPos = pC.GetCurrentAttackPos().GetComponent<Transform>();
     }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
-    }
-    private void ResetCooldown()
-    {
-        startTimer = true;
-        attackNum = 0;
-        //force = 0;
-        an.SetInteger("AttackNo", attackNum);
-    }
+
+	/*
+	 * ATTACK METHOD 
+	 *		1) VALUES
+	 *		2) CAN ATTACK CONDITION
+	 *		3) PRESS KEY CODE CONDITION
+	 *		4) CONTINUE COMBO CONDITION
+	 *			A) FIRST ATTACK
+	 *			B) SECOND ATTACK
+	 *			C) THIRD ATTACK
+	 *		5)HITTING ENEMY
+	 *			A) FIRST COLLIDER
+	 *			B) SECOND COLLIDER
+	 *			C) THIRD COLLIDER
+	 *			
+	 */ 
+	
     public void Attack()
     {
+		//Values for the attack method
         Pressed = true;
         bool attacked = true;
         Collider2D[] enemiesToDamage= null;
         Collider2D[] enemiesToDamage1=null;
         Collider2D[] enemiesToDamage2 = null;
-
+		//  if you can attack
         if (timeBtWAttack <= 0)
         {
+			//if you pressed the key code to attack 
             if (Pressed)
             {
                 Pressed = false;
                 an.SetBool("Attacking", true);
-                
+                //if you can continue the combo
                 if (timeResetAttack <= startTimeResetAttack)
                 {
                     attackNum += 1;
-
+					//if we are on the FIRST attack
                     if (attackNum == 1)
                     {
-                        an.SetInteger("AttackNo", attackNum);
-                        rigidbody.velocity = new Vector2(0f, 0f);
-                        enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
-                        
-                        force = 5f;
-
-                        Vector3 displacement = new Vector3(-1.2f, 0, 0);
-                        enemiesToDamage1 = Physics2D.OverlapCircleAll(attackPos.position - displacement, attackRange - .5f, whatIsEnemies);
-                    }
+						enemiesToDamage = AttackNumber(enemiesToDamage, enemiesToDamage1, -5f, -1.2f,0f,attackRange);
+						Debug.Log("attack 1");
+					}
+					//if we are on the SECOND attack
                     if (attackNum == 2)
                     {
-                        an.SetInteger("AttackNo", attackNum);
-                        rigidbody.velocity = new Vector2(0f, 0f);
-                        enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange2, whatIsEnemies);
-
-                        force = 15f;
-
-                        Vector3 displacement = new Vector3(-1.2f, 0, 0);
-                        enemiesToDamage1 = Physics2D.OverlapCircleAll(attackPos.position - displacement, attackRange - .5f, whatIsEnemies);
-                    }
+						Debug.Log("attack 2");
+						enemiesToDamage1 = AttackNumber(enemiesToDamage, enemiesToDamage1, 15f, -1.2f, 0f,attackRange2);
+					}
+					//if we are on the THIRD attack
                     if (attackNum == 3)
                     {
-                        
-                        an.SetInteger("AttackNo", attackNum);
+						Debug.Log("attack 3");
+						enemiesToDamage2 = AttackNumber(enemiesToDamage, enemiesToDamage1, 22f, -1.2f, 0f,attackRange3);
 
-                        rigidbody.velocity = new Vector2(0f, 0f);
-                        enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange3, whatIsEnemies);
-
-                        force = 22f;
-
-                        Vector3 displacement = new Vector3(-1.2f, 0, 0);
-                        enemiesToDamage1 = Physics2D.OverlapCircleAll(attackPos.position - displacement, attackRange - .5f, whatIsEnemies);
-                        
-                    }
+					}
                     
                     timeResetAttack = startTimeResetAttack;
                 }
-
-
-
-
-
-
-
-
-                if (enemiesToDamage != null)
+				//if you are hitting an enemy
+				if (enemiesToDamage != null)
+				{
+					Debug.Log("Hitbox 1");
+					EnemyHitBox(enemiesToDamage, attacked);
+				} else
+				{
+					Debug.Log("No hitbox 1");
+				}
+				//same as the above, with differet hitbox
+				if (enemiesToDamage1 != null)
+				{
+					Debug.Log("Hitbox 2");
+					EnemyHitBox(enemiesToDamage1, attacked);
+				}
+				//same as above with different hitbox
+				if (enemiesToDamage2 != null)
                 {
-                    for (int i = 0; i < enemiesToDamage.Length; i++)
-                    {
-                       
-                        CheckKnockBack(i,enemiesToDamage);
-                        
-                        enemiesToDamage[i].GetComponent<TakeDamageEnemy>().TakeDamage(damage);
-                        if (attacked)
-                        {
-
-                            attacked = false;
-                        }
-                        
-                    }
-                }
-                
-                if (enemiesToDamage1 != null)
-                {
-                    for (int i = 0; i < enemiesToDamage1.Length; i++)
-                    {
-                        CheckKnockBack(i, enemiesToDamage1);
-                        enemiesToDamage1[i].GetComponent<TakeDamageEnemy>().TakeDamage(damage);
-                        if (attacked)
-                        {
-                            
-                            attacked = false;
-                        }
-                        
-                    }
-                }
-                if (enemiesToDamage2 != null)
-                {
-                    for (int i = 0; i < enemiesToDamage2.Length; i++)
-                    {
-                        CheckKnockBack(i, enemiesToDamage2);
-                        enemiesToDamage2[i].GetComponent<TakeDamageEnemy>().TakeDamage(damage);
-                        if (attacked)
-                        {
-                            
-                            attacked = false;
-                        }
-                        
-                    }
-                }
-                
+					Debug.Log("Hitbox 3");
+					EnemyHitBox(enemiesToDamage2, attacked);
+                }                
                 attacked = true;
-
             }
-            timeBtWAttack = startTimeBtwAttack;
-            
+            timeBtWAttack = startTimeBtwAttack;   
         }
 
     }
+	/*
+	 * IENUMERATOR 
+	 *		1) TIME BETWEEN ATTACKS
+	 */ 
+
+
+	//IEnumerator to determine the time between attacks
     IEnumerator AttackWait(float seconds)
     {
         Debug.Log("running");
         yield return new WaitForSeconds(seconds);
 
     }
-    private void CheckKnockBack(int i, Collider2D[] enemiesToDamage)
+
+	/*
+	 * AUXILIARY METHODS
+	 *		1) ATTACK COOLDOWN
+	 *		2) KNOCKBACK
+	 *		3) CHOOSE COLLIDER
+	 * 
+	 */ 
+
+
+	// Auxiliary attack cooldown reset
+	private void ResetCooldown()
+	{
+		startTimer = true;
+		attackNum = 0;
+		//force = 0;
+		an.SetInteger("AttackNo", attackNum);
+	}
+
+	//Auxiliary private method to apply knockback
+	private void CheckKnockBack(int i, Collider2D[] enemiesToDamage)
     {
+		Debug.Log("Enemy name "+ enemiesToDamage[i].gameObject.name);
         Debug.Log(force);
         if (pC.GetAttackPos())
         {
@@ -203,9 +208,50 @@ public class PlayerAttack : MonoBehaviour
         {
             Debug.Log("Izquierda");
             enemiesToDamage[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(-1, 2) * force);
-            //enemiesToDamage[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(-1, 2) * force);
         }
         
     }
-    
+
+	//Auxiliary method to determine the collider 
+	private void EnemyHitBox(Collider2D[] enemiesToDamage, bool attacked)
+	{
+		// check every enemy hit
+		for (int i = 0; i < enemiesToDamage.Length; i++)
+		{
+			//Apply knockback
+			CheckKnockBack(i, enemiesToDamage);
+			enemiesToDamage[i].GetComponent<TakeDamageEnemy>().TakeDamage(damage);
+			// if you attack, dont apply again
+			if (attacked)
+			{
+				attacked = false;
+			}
+
+		}
+	}
+	//Auxiliary method for number of attack
+	private Collider2D[] AttackNumber(Collider2D[] enemiesToDamage, Collider2D[] enemiesToDamage1, float applyForce, float xDis, float rbVelocity, float att)
+	{
+		an.SetInteger("AttackNo", attackNum);
+		rigidbody.velocity = new Vector2(rbVelocity, 0f);
+		enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, att, whatIsEnemies);
+
+		force = applyForce;
+
+		Vector3 displacement = new Vector3(xDis,0, 0);
+		enemiesToDamage1 = Physics2D.OverlapCircleAll(attackPos.position - displacement, attackRange - .5f, whatIsEnemies);
+		return enemiesToDamage;
+	}
+
+	/*
+	 * GRIZMOS DRAWING
+	 */ 
+
+	//Drawing private method hit box for the attack
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(attackPos.position, attackRange);
+	}
+
 }
--- Figthing Platformer/Assets/Scripts/PlayerMovement/PlayerController.cs
+++ Figthing Platformer/Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -1,48 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ *  GLOSARY:
+ *		A) INSTANCE VALUES
+ *			1) COMPONETS
+ *			2) MOVEMENT VALUES
+ *			3) JUMP COUNTERS
+ *			4) BOOLEANS
+ *			5) GAME OBJECTS, ANIMATOR AND SCRIPTS
+ *	
+ *		B) START METHOD
+ *		
+ *		C) UPDATE METHOD
+ *			1) MOVEMENT GET AXIS VALUES
+ *			2) KEY CODES
+ *			3) VELOCITIY SCENARIOS
+ *			4) ANIMATIONS
+ *		
+ *		D) MOVEMENT METHODS
+ *		
+ *		E) IENUMERATORS
+ *		
+ *		F) AUXILIARY METHODS
+ * 
+ */ 
 
 public class PlayerController : MonoBehaviour
 {
-
+	//Components used for the main player
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     public Collision coll;
 
-
+	//Public values for all the movement used in the code
     public float moveSpeed = 5.0f;
     public float distance = 1f;
     public float jumpVelocity = 10f;
     public float fallMultiplier = 4f;
     public float lowJumpMultiplier = 3f;
     public float slideDown = -5f;
-    public int numJumps = 2;
     public float jumpPushForce = 10f;
-    public int side = 1;
     public float wallJumpLerp = 10f;
-
     public float wallJumpForce = 30f;
     public float dashSpeed = 20;
-
-
-    public bool canMove;
+	//counter for amount of jumps available for the player
+	public int numJumps = 2;
+	//Booleans for to determine different available movements
+	public bool canMove;
     public bool wallGrab;
     public bool wallJumped;
     public bool wallSlide;
     public bool isDashing;
-
-
-
     public bool hasDashed;
-
     public bool groundTouch;
+	private bool attackPos;
 
-    public GameObject attackPosMeleeLeft, attackPosMeleeRight;
-
+	//Game objects for the left position and right position of the hitbox
+	public GameObject attackPosMeleeLeft, attackPosMeleeRight;
+	//Game object determining the side his facing
     private GameObject CurrentAttackPos;
-    private bool attackPos;
+    //Animators used for the movements
     private Animator an;
+	//Attack Script
     private PlayerAttack plA;
 
 
@@ -60,37 +80,76 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
 
-        //Movement
+        //Movement values for getting the Axis pressed
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         float xRaw = Input.GetAxisRaw("Horizontal");
         float yRaw = Input.GetAxisRaw("Vertical");
-        Vector2 dir = new Vector2(x, y);
-        Walk(dir);
 
-
-        //Better Jumping
-        if (Input.GetKeyDown(KeyCode.W) && numJumps > 0 && !coll.onWall)
+		/*
+		 * KEY CODES FOR MOVEMENTS, JUMPS AND GRABS
+		 *		1) WALK METHOD
+		 *		2) JUMPING
+		 *		3) WALL JUMPING
+		 *		4) WALL GRAB
+		 *		5) LINE 103: DASH
+		 */
+	
+		//Get the vector for the movement pressed and call the walk method
+		Vector2 dir = new Vector2(x, y);
+		Walk(dir);
+		//key code Better Jumping
+		if (Input.GetKeyDown(KeyCode.W) && numJumps > 0 && !coll.onWall)
         {
             an.SetInteger("JumpNo", numJumps);
-
             Jump(Vector2.up);
-
             numJumps--;
         }
-
-
-
-
+		// key code for wall jumping
         if (Input.GetKeyDown(KeyCode.W) && coll.onWall)
         {
             WallJump();
 
         }
-        if (coll.onWall)
+		// key code for wall grab
+		if (Input.GetKey(KeyCode.E) && coll.onWall)
+		{
+			Debug.Log("WALL GRAB");
+			rb.velocity = new Vector2(rb.velocity.x, 0);
+		}
+		// key code for dash
+		if (Input.GetKeyDown(KeyCode.Space) && hasDashed == false && !coll.onGround)
+		{
+			if (xRaw != 0 || yRaw != 0)
+				Dash(xRaw, yRaw);
+			numJumps = 0;
+		}
+		/*
+		 * 
+		 * Velocity control for player falling in different scenarios 
+		 *		
+		 */
+		if (rb.velocity.y < 0)
+		{
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		}
+		else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.W))
+		{
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+		/*
+		 * ANIMATIONS FOR PLAYER MOVEMENTS
+		 *		1) WALL GRAB ANIMATION
+		 *		2) JUMPS RESETS ANIMATIONS AND BOOLEAN CONTROLS
+		 *		3) JUMPS ANIMATIONS
+		 *		4) FALLING ANIMATION
+		 *		
+		 */ 
+
+		//Animation for wall grab
+		if (coll.onWall)
         {
             an.SetBool("WallGrab", true);
         }
@@ -98,33 +157,8 @@ public class PlayerController : MonoBehaviour
         {
             an.SetBool("WallGrab", false);
         }
-
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }
-        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.W))
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-        }
-
-        if (Input.GetKeyDown(KeyCode.G) && !coll.onGround)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-        }
-
-
-
-        if (Input.GetKeyDown(KeyCode.Space) && hasDashed == false && !coll.onGround)
-        {
-            if (xRaw != 0 || yRaw != 0)
-                Dash(xRaw, yRaw);
-            numJumps = 0;
-        }
-
-        //walljump tryouts 
-
-        // jump reset
+		
+        // jump reset animations/booleans
         if (coll.onGround)
         {
             an.SetBool("Jumping", false);
@@ -136,8 +170,29 @@ public class PlayerController : MonoBehaviour
             hasDashed = false;
             wallJumped = true;
         }
+		//jumping animations for double and single jump
+		if (!coll.onGround)
+		{
+			an.SetBool("Jumping", true);
+			an.SetBool("DoubleJump", true);
+			an.SetInteger("JumpNo", numJumps);
 
-        if (!coll.onGround && rb.velocity.y < 0)
+			if (numJumps == 2)
+			{
+				numJumps = 1;
+			}
+			if (numJumps == 1)
+			{
+				an.SetBool("DoubleJump", true);
+			}
+			if (numJumps == 0)
+			{
+				an.SetBool("DoubleJump", false);
+			}
+
+		}
+		// falling animation 
+		if (!coll.onGround && rb.velocity.y < 0)
         {
             an.SetBool("Falling", true);
         }
@@ -145,50 +200,40 @@ public class PlayerController : MonoBehaviour
         {
             an.SetBool("Falling", false);
         }
-
-        if (!coll.onGround)
-        {
-            an.SetBool("Jumping", true);
-            an.SetBool("DoubleJump", true);
-            an.SetInteger("JumpNo", numJumps);
-
-            if (numJumps == 2)
-            {
-                numJumps = 1;
-
-            }
-
-            if (numJumps == 1)
-            {
-                //an.SetBool("Falling", false);
-                an.SetBool("DoubleJump", true);
-
-            }
-
-            if (numJumps == 0)
-            {
-                an.SetBool("DoubleJump", false);
-
-            }
-
-        }
-        /*
-                if (coll.onWall && !coll.onGround)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, slideDown);
-                }
-        */
         AttackCheck();
     }
+	/*
+	 * METHODS FOR PLAYER MOVEMENTS (OUTSIDE UPDATE METHOD)
+	 *		1) DASH
+	 *		2) JUMPING
+	 *		3) WALK
+	 *		4) WALL JUMPING
+	 * 
+	 */
 
-    private void AttackCheck()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            plA.Attack();
-        }
-    }
-    private void Walk(Vector2 dir)
+	//private method for dash 
+	private void Dash(float x, float y)
+	{
+
+		hasDashed = true;
+
+		rb.velocity = Vector2.zero;
+		Vector2 dir = new Vector2(x, y);
+
+		rb.velocity += dir.normalized * dashSpeed;
+		StartCoroutine(DashWait());
+	}
+	//private method for jumping
+	private void Jump(Vector2 dir)
+	{
+
+		rb.velocity = new Vector2(rb.velocity.x, 0);
+		rb.velocity += dir * jumpVelocity;
+		Debug.Log("DO " + rb.velocity);
+
+	}
+	// private method for walk 
+	private void Walk(Vector2 dir)
     {
         if (canMove == true)
         {
@@ -222,52 +267,36 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+	
+	//private method for wall jumping
+	private void WallJump()
+	{
+		StopCoroutine(DisableMovement(0));
+		StartCoroutine(DisableMovement(.1f));
+		Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+		Jump(Vector2.up + wallDir);
+		wallJumped = true;
 
-    IEnumerator DisableMovement(float time)
+	}
+
+
+	/*
+	 * IENUMERATORS FOR COUNTING TIME 
+	 *		1) DISABLE MOVEMENT
+	 *		2) DASH WAIT 
+	 *		3) GROUND DASH (USED INSIDE DASH AND WAIT)
+	 * 
+	 */ 
+
+	// IEnumerator for disable movement on the wallJumpo method
+	IEnumerator DisableMovement(float time)
     {
         canMove = false;
         yield return new WaitForSeconds(time);
         canMove = true;
     }
 
-    private void Jump(Vector2 dir)
-    {
-
-        rb.velocity = new Vector2(rb.velocity.x, 0);
-        rb.velocity += dir * jumpVelocity;
-        Debug.Log("DO " + rb.velocity);
-
-    }
-
-    private void WallJump()
-    {
-
-        StopCoroutine(DisableMovement(0));
-        StartCoroutine(DisableMovement(.1f));
-        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
-
-        Jump(Vector2.up + wallDir);
-
-        //Debug.Log("send " + wallDir);
-
-        wallJumped = true;
-
-    }
-
-
-
-    private void Dash(float x, float y)
-    {
-        
-        hasDashed = true;
-
-        rb.velocity = Vector2.zero;
-        Vector2 dir = new Vector2(x, y);
-
-        rb.velocity += dir.normalized * dashSpeed;
-        StartCoroutine(DashWait());
-    }
-
+	//IEnumarator for the private dash method
     IEnumerator DashWait()
     {
         FindObjectOfType<GhostTrail>().ShowGhost();
@@ -286,7 +315,24 @@ public class PlayerController : MonoBehaviour
         wallJumped = false;
         isDashing = false;
     }
-    private void flipAttack(bool t)
+	//IEnumerator for the method IEnumerator in DashAndWait
+	IEnumerator GroundDash()
+	{
+		yield return new WaitForSeconds(.15f);
+
+	}
+
+	/*
+	 * AUXILIARY METHODS FOR THE SCRIPTS
+	 *		1) FLIP ATTACK 
+	 *		2) GET CURRENT ATTACK POSITION
+	 *		3) GET ATTACK POSITION
+	 *		4) KEY CODE FOR ATTACK CKECK
+	 * 
+	 */ 
+
+	//Auxiliary private method to determine the position in which the main player is facing
+	private void flipAttack(bool t)
     {
         if (t)
         {
@@ -298,32 +344,28 @@ public class PlayerController : MonoBehaviour
             CurrentAttackPos = attackPosMeleeLeft;
             attackPos = false;
         }
-
-        //AttackPosMelee.GetComponent<Transform>().position = new Vector3(-AttackPosMelee.GetComponent<Transform>().position.x, -AttackPosMelee.GetComponent<Transform>().position.y, 0);
     }
+	//Auxiliary public method to get the current attack position 
     public GameObject GetCurrentAttackPos()
     {
         return CurrentAttackPos;
     }
+	//Auxiliary public method to get the an attack position
     public bool GetAttackPos()
     {
         return attackPos;
     }
 
+	//Auxiliary key code private method for the atack check
+	private void AttackCheck()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Debug.Log("PC attack");
+			plA.Attack();
+		}
+	}
 
-    IEnumerator GroundDash()
-    {
-        yield return new WaitForSeconds(.15f);
-
-    }
-
-
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.layer == 9)
-            gameObject.transform.position = new Vector3(0f, -1.485f, 0f);
-    }
 
 
 }
